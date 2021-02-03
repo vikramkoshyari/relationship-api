ca979c0ed832588733d9a2d08b8fe15eeb85ba18
vikram singh koshyari <vikramkoshyari222@gmail.com>
2021-02-03 12:22:46 +0530

    get2

--- routes.js
+++ routes.js
@@ -35,6 +35,19 @@ router.post('/persons/:name',(req,res)=>{
         res.send('success')
     })
 })
-
+// insert into relationship-type
+router.post('/relationship-type/:relation',(req,res)=>{
+    con.query('insert into relationship_type (relation) values ("'+req.params.relation+'")',(err,result)=>{
+        if(err)throw err;
+        console.log("success")
+    })
+})
+// insert into relationship table
+router.post('/relationship/:personID/:relativeID/:relation',(req,res)=>{
+    con.query('insert into relationship (personID,relativeID,relation) values (\''+req.params.personID+'\',\''+req.params.relativeID+'\',\''+req.params.relation+'\')',(err,result)=>{
+        if(err)throw err;
+        res.send("success")
+    })
+})
 
 module.exports=router
