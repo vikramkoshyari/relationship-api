fa5d40185a7f4367cb0ff222ef002c36df3455f0
vikram singh koshyari <vikramkoshyari222@gmail.com>
2021-02-13 19:48:48 +0530

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if(port==null){
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://http://relationshipbuilder.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'http://relationshipbuilder.herokuapp.com/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
